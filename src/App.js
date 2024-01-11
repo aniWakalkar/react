32f13c0112cb368f47da08678ff4248eca2b737a
aniWakalkar <aniketwakalkar23@gmail.com>
2024-01-11 22:23:03 +0530

    myPortfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import home from './img/home.jpg'
+import cv from './img/myCV.pdf'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar/>
+    <div className="container-fluid p-0 mt-5">
+      <div className="container mt-4 p-4 mb-5" style={{display: "flex", alignItems: "center", backgroundColor: "aliceblue", boxShadow: "3px 4px 5px"}}>
+          <img src={home} alt="key_image"/>
+          <h6 className="display-6" style={{marginLeft: '30px'}}>
+            You are the <b>master</b> of <b>your destiney,</b> you can <b>influence direct</b> and <b>control</b> your own environment you can make <b>your life</b> what <b> you want it to be.</b> <br/>
+            <a className="btn btn-success mt-5" href={cv} download="myCV" role="button">Download CV</a>
+          </h6>
+      </div>
     </div>
+    </>
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import icon from '../img/profile.png'
+function Navbar() {
+  return (
+    <>
+        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+            <div className='container-fluid'>
+                <a className="navbar-brand mx-5" href="index.html">Portfolio</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/">Home</a>
+                        </li>
+                    </ul>
+                    <form className="d-flex" style={{alignItems: 'center', marginRight: '50px'}}>
+                        <h4 className="text-success" style={{marginRight:'50px'}}>Aniket Wakallkar </h4>
+                        <img src={icon} alt="key_image" style={{height:'50px', width: '50px'}}/>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
